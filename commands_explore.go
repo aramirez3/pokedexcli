2ad17064cf9034a24963436e9bd6a4f5616035a8
Angel Ramirez <angelra@gmail.com>
2024-11-05 23:43:18 -0800

    cached explored areas

--- commands_explore.go
+++ commands_explore.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aramirez3/pokedexcli/internal/pokeapi"
 )
 
 func commandExplore(cfg *config) error {
@@ -12,13 +14,24 @@ func commandExplore(cfg *config) error {
 		fmt.Println("explore requires an area-name")
 		return nil
 	}
-
+	cached, found := cfg.Cache.Get(areaName)
+	if found {
+		locationExplored := pokeapi.LocationAreaExplored{}
+		pokeapi.UnmarshalData(cached, &locationExplored)
+		printLocationExplored(locationExplored)
+		return nil
+	}
 	locationExplored, _ := cfg.pokeapiClient.ExploreLocation(&areaName)
-	fmt.Println(locationExplored)
+	byteData, _ := pokeapi.MarshalData(locationExplored)
+	cfg.Cache.Add(areaName, byteData)
+	printLocationExplored(locationExplored)
+	return nil
+}
+
+func printLocationExplored(locationExplored pokeapi.LocationAreaExplored) {
 	fmt.Printf("Exploring %s...\n", locationExplored.Name)
 	fmt.Println("Found Pokemon:")
 	for _, encounter := range locationExplored.PokemonEncounters {
 		fmt.Printf(" - %s\n", encounter.Pokemon.Name)
 	}
-	return nil
 }
